e74f473276561b5b3bd6911f4409c3c4054d3157
rerikalilithfs <rerikalfsilva@gmail.com>
2022-05-12 02:35:21 -0300

    Consulta simples com query methods

--- src/main/java/com/rerikalfs/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rerikalfs/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package com.rerikalfs.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rerikalfs.workshopmongo.domain.Post;
-import com.rerikalfs.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
